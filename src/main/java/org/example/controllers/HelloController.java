aee9c15319b1d3586cdc84a79f2a4a8c0a99dfb2
Terrel <TerrelRJones@gmail.com>
2021-12-12 10:44:48 -0800

    added spring boot main method and hello

--- src/main/java/org/example/controllers/HelloController.java
+++ src/main/java/org/example/controllers/HelloController.java
@@ -1,2 +1,13 @@
-package org.example.controllers;public class HelloController {
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "index";
+    }
 }
